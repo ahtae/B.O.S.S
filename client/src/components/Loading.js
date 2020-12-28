597e0fb8aecda88de2118e8e43a71f301eb2f91f
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 13:26:22 -0500

    refactor: Loading component

---
+++ client/src/components/Loading.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+import styles from '../utils/styles/loading';
+
+const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <Spinner
+        visible={true}
+        size="large"
+        textContent="Loading..."
+        color="gray"
+        textStyle={styles.spinnerTextStyle}
+      />
+    </View>
+  );
+};
+
+export default Loading;
