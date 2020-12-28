1eb76b48d275baec43a7c80ffff5d4065ada1c92
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 15:19:49 -0500

    refactor: UpdateComment component

---
+++ client/src/components/commentsList/comments/updateComment/UpdateComment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import Comment from '../comment/Comment';
+
+const UpdateComment = ({ comment }) => {
+  const output = comment ? (
+    <View>
+      <Comment information={comment} type="update" />
+    </View>
+  ) : null;
+
+  return <View>{output}</View>;
+};
+
+export default UpdateComment;
