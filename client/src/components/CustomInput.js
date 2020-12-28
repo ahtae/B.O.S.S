b570f4c46f991d58ff5df7d73e39dcd8bc7b3d6b
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 13:43:14 -0500

    feat: Create a CustomInput component and add styling

---
+++ client/src/components/CustomInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, TextInput } from 'react-native';
+import styles from '../utils/styles/customInput';
+
+const CustomInput = (props) => {
+  const {
+    field: { name, onBlur, onChange, value },
+    form: { errors, touched, setFieldTouched },
+    ...inputProps
+  } = props;
+
+  const hasError = errors[name] && touched[name];
+
+  return (
+    <>
+      <TextInput
+        style={[styles.textInput, hasError && styles.errorInput]}
+        value={value}
+        onChangeText={(text) => onChange(name)(text)}
+        onBlur={() => {
+          setFieldTouched(name);
+          onBlur(name);
+        }}
+        {...inputProps}
+      />
+      {hasError && <Text style={styles.errorText}>{errors[name]}</Text>}
+    </>
+  );
+};
+
+export default CustomInput;
---
+++ client/src/utils/styles/customInput.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    width: '80%',
+    margin: 10,
+    backgroundColor: 'rgb(245, 245, 245)',
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    padding: 5
+  },
+  errorText: {
+    fontSize: 10,
+    color: 'red'
+  },
+  errorInput: {
+    borderColor: 'red'
+  }
+});
+
+export default styles;
