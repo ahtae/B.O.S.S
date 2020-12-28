141c1dc013806334f70b8ba6d8094c05c79f9877
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 14:41:50 -0500

    refactor: Business component

---
+++ client/src/components/businesses/business/Business.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import BusinessCard from './businessCard/BusinessCard';
+
+const Business = ({ business, navigation }) => {
+  const navigateToBusiness = () => {
+    navigation.navigate('Business', { id: business.id });
+  };
+
+  return (
+    <TouchableOpacity activeOpacity={1.0} onPress={navigateToBusiness}>
+      <BusinessCard business={business} />
+    </TouchableOpacity>
+  );
+};
+
+export default Business;
