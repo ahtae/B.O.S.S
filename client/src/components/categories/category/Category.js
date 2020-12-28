7ed548aea32b9217696163dd24b912d8a019ba4e
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 14:31:56 -0500

    feat: Create a Category screen and add styling

---
+++ client/src/components/categories/category/Category.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, Image, TouchableOpacity } from 'react-native';
+import styles from '../../../utils/styles/category';
+
+const Category = ({ category, selected, handleCategoryClick }) => {
+  return (
+    <TouchableOpacity
+      key={category.label}
+      style={{
+        ...styles.container,
+        backgroundColor: selected === category.label ? 'lightgray' : 'white',
+        borderRadius: '13%'
+      }}
+      onPress={() => handleCategoryClick(category.label)}
+    >
+      <Text style={styles.text}>{category.label}</Text>
+      <Image source={category.icon} style={styles.image} alt={category.label} />
+    </TouchableOpacity>
+  );
+};
+
+export default Category;
