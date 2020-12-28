b85c34537d50b6a696b87a7aee4f6fdcf3bd4869
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 14:52:34 -0500

    refactor: User component

---
+++ client/src/components/User.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Card, Title, Paragraph, IconButton } from 'react-native-paper';
+import { removeUserFromServer } from '../store/users';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from '../utils/styles/user';
+
+const User = ({ currentUser }) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const { firstName, lastName, email, image } = currentUser;
+
+  const handleRemoveClick = (id) => {
+    dispatch(removeUserFromServer(id));
+  };
+
+  return (
+    <View style={styles.backgroundStyle}>
+      {user && user.isAdmin ? (
+        <IconButton
+          size={20}
+          icon="close"
+          onPress={() => handleRemoveClick(currentUser.id)}
+          accessibilityLabel="close"
+        />
+      ) : null}
+      <Title style={styles.textStyle}>{`${firstName} ${lastName}`}</Title>
+      <Paragraph style={styles.paragraphStyle}>{`${email} \n`}</Paragraph>
+      <Card.Cover style={styles.imageStyle} source={{ uri: `${image}` }} />
+    </View>
+  );
+};
+
+export default User;
