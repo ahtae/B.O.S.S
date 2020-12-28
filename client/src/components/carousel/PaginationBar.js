c9d524498aa46594f0257b2aa77e29d0e49d9c6e
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 13:38:25 -0500

    refactor: Styling

---
+++ client/src/components/carousel/PaginationBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Pagination } from 'react-native-snap-carousel';
+import CarouselPaginationBar from './CarouselPaginationBar';
+import styles from '../../utils/styles/paginationBar';
+
+const PaginationBar = ({ activeSlide, images, carouselRef }) => {
+  return (
+    <Pagination
+      carouselRef={carouselRef}
+      dotsLength={images ? images.length : 0}
+      activeDotIndex={activeSlide}
+      dotStyle={styles.dot}
+      inactiveDotOpacity={0.4}
+      inactiveDotScale={0.6}
+      dotElement={
+        <CarouselPaginationBar width={10} carouselRef={carouselRef} />
+      }
+      inactiveDotElement={
+        <CarouselPaginationBar width={10} carouselRef={carouselRef} inactive />
+      }
+    />
+  );
+};
+
+export default PaginationBar;
---
+++ client/src/utils/styles/paginationBar.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 8,
+    backgroundColor: 'lightgray'
+  }
+});
+
+export default styles;
