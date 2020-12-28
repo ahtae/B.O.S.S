35ff119b98fa292b7b5cab043371402e8ff5e3ef
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 15:28:26 -0500

    chore: Import and export components

---
+++ client/src/components/index.js
@@ -0,0 +1,19 @@
+import CommentsList from './commentsList/CommentsList';
+import Businesses from './businesses/Businesses';
+import BusinessCard from './businesses/business/businessCard/BusinessCard';
+import CarouselOfImages from './carousel/CarouselOfImages';
+import Search from './Search';
+import Categories from './categories/Categories';
+import Logo from './Logo';
+import Loading from './Loading';
+
+export {
+  CommentsList,
+  CarouselOfImages,
+  Loading,
+  Businesses,
+  Search,
+  Categories,
+  BusinessCard,
+  Logo
+};
