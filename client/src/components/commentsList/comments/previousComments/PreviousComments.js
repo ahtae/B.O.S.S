822caa16b322f22a18e3ff84ae485ab16b94d0b7
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 15:18:32 -0500

    refactor: PreviousComments component

---
+++ client/src/components/commentsList/comments/previousComments/PreviousComments.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import Comment from '../comment/Comment';
+
+const PreviousComments = ({ comments }) => {
+  const output = comments.length
+    ? comments.map((comment) => (
+        <View key={comment.id}>
+          <Comment information={comment} type="previous" />
+        </View>
+      ))
+    : null;
+
+  return <View>{output}</View>;
+};
+
+export default PreviousComments;
