1599ec761df903e98d925fbe436b9f04714d9e34
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 15:10:09 -0500

    refactor: CommentsList component

---
+++ client/src/components/commentsList/CommentsList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Title, Paragraph } from 'react-native-paper';
+import Comment from './comments/comment/Comment';
+import Comments from './comments/Comments';
+import { Rating } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import styles from '../../utils/styles/commentsList';
+
+const CommentsList = ({ arrayOfComments, business, navigation }) => {
+  const user = useSelector((state) => state.user);
+
+  const output = arrayOfComments.length ? (
+    <View>
+      {arrayOfComments.map((comments) => {
+        return comments.length === 1 ? (
+          <View key={comments[0].id} style={styles.containerStyle}>
+            <Comment information={comments[0]} type="regular" />
+          </View>
+        ) : (
+          <Comments key={comments[0].id} comments={comments} />
+        );
+      })}
+    </View>
+  ) : null;
+
+  return (
+    <View style={styles.backgroundStyle}>
+      <Title style={styles.textStyle}>Recommended Reviews</Title>
+      {user && user.isAdmin ? null : (
+        <View style={styles.containerStyle}>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('Comment Form', { business: business })
+            }
+          >
+            <Rating
+              type="custom"
+              ratingCount={5}
+              imageSize={20}
+              ratingColor="lightgray"
+              ratingTextColor="lightgray"
+              startingValue={0}
+              readonly={true}
+              style={styles.starStyle}
+            />
+            <Paragraph style={styles.paragraphStyle}>
+              Tap to review...
+            </Paragraph>
+          </TouchableOpacity>
+        </View>
+      )}
+      {output}
+    </View>
+  );
+};
+
+export default CommentsList;
