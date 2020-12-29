d2c21c297fd2f356e98dbf9f11f47cd19913a8fe
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 23:31:25 -0500

    chore: Import and export screens

---
+++ client/src/screens/index.js
@@ -0,0 +1,30 @@
+// import BusinessCard from './BusinessCard';
+import BusinessesList from './BusinessesList';
+import BusinessOwnerProfile from './BusinessOwnerProfile';
+import LoginForm from './LoginForm';
+import UserSignUpForm from './UserSignUpForm';
+import Home from './Home';
+import SingleBusiness from './SingleBusiness';
+import OwnerSignUpForm from './OwnerSignUpForm';
+import CommentForm from './CommentForm';
+import LocationAndTypeFilter from './LocationAndTypeFilter';
+import LocationFilter from './LocationFilter';
+import TypeFilter from './TypeFilter';
+import Prompt from './Prompt';
+import Options from './Options';
+
+export {
+  BusinessOwnerProfile,
+  LocationAndTypeFilter,
+  LocationFilter,
+  TypeFilter,
+  BusinessesList,
+  UserSignUpForm,
+  OwnerSignUpForm,
+  CommentForm,
+  Options,
+  Home,
+  LoginForm,
+  SingleBusiness,
+  Prompt
+};
