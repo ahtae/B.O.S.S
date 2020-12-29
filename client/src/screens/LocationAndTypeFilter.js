e9fc05e4f9cba31f31b005c752a2a3374d04cd89
Kristy Li <kristyli20.0@gmail.com>
2020-12-29 09:51:08 -0500

    feat: Create a LocationAndTypeFilter component and add styling

---
+++ client/src/screens/LocationAndTypeFilter.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import * as Location from "expo-location";
+import { Categories, Search } from "../components/index";
+import styles from "../utils/styles/locationAndTypeFilter";
+import * as Permissions from "expo-permissions";
+import config from "../../../config";
+
+const LocationAndTypeFilter = ({ navigation }) => {
+  const [selected, setSelected] = useState("All Businesses");
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
+  const [location, setLocation] = useState(null);
+
+  const getCurrentLocationHook = async () => {
+    try {
+      await Permissions.askAsync(Permissions.LOCATION);
+
+      const currentLocation = await Location.getCurrentPositionAsync();
+
+      setLocation({
+        latitude: `${currentLocation.coords.latitude}`,
+        longitude: `${currentLocation.coords.longitude}`,
+      });
+    } catch (err) {
+      setError("Permission to access location was denied!");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  const getAddressFromCoordinates = async (latitude, longitude) => {
+    try {
+      const url = `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=${config.HERE_API_KEY}&mode=retrieveAddresses&prox=${latitude},${longitude}`;
+      const response = await fetch(url);
+      const responseData = await response.json();
+
+      if (
+        responseData &&
+        responseData.Response &&
+        responseData.Response.View &&
+        responseData.Response.View[0] &&
+        responseData.Response.View[0].Result &&
+        responseData.Response.View[0].Result[0]
+      ) {
+        setSearch(
+          responseData.Response.View[0].Result[0].Location.Address.City
+        );
+      }
+    } catch (err) {
+      setError("Something went wrong!");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentLocationHook();
+  }, []);
+
+  useEffect(() => {
+    if (location && location.latitude && location.longitude) {
+      getAddressFromCoordinates(location.latitude, location.longitude);
+    }
+  }, [location]);
+
+  const handleSearchUpdate = (event) => {
+    setSearch(event);
+  };
+
+  const resetSearchValue = () => {
+    setSearch("");
+  };
+
+  const handleCategoryClick = (label) => {
+    setSelected(label);
+
+    setTimeout(() => {
+      setError("");
+    }, 5000);
+  };
+
+  const handleSubmitClick = () => {
+    if (selected && search.trim().length) {
+      navigation.navigate("All Businesses", {
+        searchLocation: search.trim(),
+        category: selected,
+      });
+      resetSearchValue();
+    } else {
+      setError("Location was not specified!");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Filter Businesses</Text>
+      <View>
+        <Search search={search} handleSearchUpdate={handleSearchUpdate} />
+        <Text style={styles.errorText}>{error}</Text>
+        <Categories
+          selected={selected}
+          handleCategoryClick={handleCategoryClick}
+        />
+        <TouchableOpacity
+          style={{
+            ...styles.button,
+            backgroundColor: !search.trim().length ? "lightgray" : "#5fb7fe",
+          }}
+          onPress={handleSubmitClick}
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default LocationAndTypeFilter;
---
+++ client/src/utils/styles/locationAndTypeFilter.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    height: "100%",
+  },
+  text: {
+    marginTop: 40,
+    marginLeft: 15,
+    fontFamily: "Roboto_300Light",
+    textAlign: "center",
+    fontSize: 30,
+    backgroundColor: "white",
+    marginBottom: 10,
+  },
+  button: {
+    alignSelf: "center",
+    padding: "5%",
+    marginVertical: 15,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 2,
+    width: 350,
+  },
+  buttonText: {
+    fontFamily: "Roboto_700Bold",
+    color: "black",
+    fontSize: 15,
+    textAlign: "center",
+  },
+  errorText: { color: "red", fontWeight: "bold", textAlign: "center" },
+});
+
+export default styles;
