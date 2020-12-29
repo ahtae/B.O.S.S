cfdf8338b757d982f1c113768f127a6b02aa29e1
Kristy Li <kristyli20.0@gmail.com>
2020-12-29 10:16:16 -0500

    feat: Determine the region of the businesses

--- client/src/screens/BusinessesList.js
+++ client/src/screens/BusinessesList.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Text, SafeAreaView } from 'react-native';
-import { Title } from 'react-native-paper';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-import { Businesses } from '../components/index';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import { fetchBusinessesFromServer } from '../store/businesses';
-import { Loading } from '../components';
-import styles from '../utils/styles/businessLists';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text, SafeAreaView } from "react-native";
+import { Title } from "react-native-paper";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import { Businesses } from "../components/index";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { fetchBusinessesFromServer } from "../store/businesses";
+import { Loading } from "../components";
+import styles from "../utils/styles/businessLists";
+import { useDispatch, useSelector } from "react-redux";
 
-const BusinessesList = ({ navigation }) => {
+const BusinessesList = ({ route, navigation }) => {
+  const { searchLocation, category } = route.params;
   const businesses = useSelector((state) => state.businesses);
   const dispatch = useDispatch();
   const [markers, setMarkers] = useState([]);
-  const [location, setLocation] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const [averageLatitude, setAverageLatitude] = useState(null);
+  const [averageLongitude, setAverageLongitude] = useState(null);
 
   const getAllBusinessesHook = () => {
     try {
-      dispatch(fetchBusinessesFromServer());
+      dispatch(fetchBusinessesFromServer(searchLocation, category));
     } catch (err) {
-      setErrorMessage('Something went wrong!');
+      setErrorMessage("Something went wrong!");
+      console.log(err);
     }
   };
 
@@ -31,60 +34,64 @@ const BusinessesList = ({ navigation }) => {
       title: business.name,
       images: business.images[0],
       longitude: Number(business.longitude),
-      latitude: Number(business.latitude)
+      latitude: Number(business.latitude),
     }));
 
     setMarkers(arrayOfDirections);
   };
 
-  const getCurrentLocationHook = async () => {
-    try {
-      await Permissions.askAsync(Permissions.LOCATION);
+  const determineRegion = () => {
+    const longitude = businesses.length
+      ? businesses.reduce(
+          (sum, business) => Number(business.longitude) + sum,
+          0
+        ) / businesses.length
+      : 0;
+    setAverageLongitude(longitude);
 
-      const currentLocation = await Location.getCurrentPositionAsync();
-
-      setLocation(currentLocation);
-    } catch (err) {
-      setErrorMessage('Permission to access location was denied!');
-    }
+    const latitude = businesses.length
+      ? businesses.reduce(
+          (sum, business) => Number(business.latitude) + sum,
+          0
+        ) / businesses.length
+      : 0;
+    setAverageLatitude(latitude);
   };
 
   useEffect(() => {
-    getCurrentLocationHook();
-
-    if (!errorMessage) {
-      getAllBusinessesHook();
-    }
+    getAllBusinessesHook();
   }, []);
 
   useEffect(() => {
+    determineRegion();
     createMarkersHook();
   }, [businesses]);
 
-  const output = location ? (
-    <View>
-      <MapView
-        style={styles.mapStyle}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation={true}
-        region={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1
-        }}
-        zoomEnabled={true}
-        scrollEnabled={true}
-        showCompass={true}
-        rotateEnabled={false}
-      >
-        {markers.map((marker) => (
-          <Marker key={marker.id} coordinate={marker} title={marker.title} />
-        ))}
-      </MapView>
-      <Businesses navigation={navigation} businesses={businesses} />
-    </View>
-  ) : null;
+  const output =
+    averageLatitude && averageLongitude ? (
+      <View>
+        <MapView
+          style={styles.mapStyle}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={false}
+          region={{
+            latitude: averageLatitude,
+            longitude: averageLongitude,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+          zoomEnabled={true}
+          scrollEnabled={true}
+          showCompass={true}
+          rotateEnabled={false}
+        >
+          {markers.map((marker) => (
+            <Marker key={marker.id} coordinate={marker} title={marker.title} />
+          ))}
+        </MapView>
+        <Businesses navigation={navigation} businesses={businesses} />
+      </View>
+    ) : null;
 
   return (
     <SafeAreaView>
