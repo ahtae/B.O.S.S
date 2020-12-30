702e07f3346565631524d3bf814f4ae5f5e572f6
Kristy Li <kristyli20.0@gmail.com>
2020-12-30 09:12:30 -0500

    refactor: Comment component

--- client/src/components/commentsList/comments/comment/Comment.js
+++ client/src/components/commentsList/comments/comment/Comment.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Paragraph, IconButton } from 'react-native-paper';
 import { View, Text, Image } from 'react-native';
 import { Rating } from 'react-native-elements';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { removeCommentFromServer } from '../../../../redux/store/comments';
-import { useDispatch } from 'react-redux';
+import { removeCommentFromServer } from '../../../../redux/actions/comments';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from '../../../../utils/styles/comment';
 
-const Comment = (props) => {
+const Comment = ({ information, type }) => {
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const { removeComment, information, type } = props;
   const {
     id,
     title,
@@ -17,55 +16,79 @@ const Comment = (props) => {
     stars,
     photo,
     createdAt,
-    updatedAt,
-    user
+    updatedAt
   } = information;
-  const { firstName, lastName, image } = user;
+  const commentUser = information.user;
+  const { firstName, lastName, image } = commentUser;
 
   const typeOutput =
-    type === 'update' ? (
-      <MaterialCommunityIcons name="update" size={15} color="black" />
-    ) : null;
-  const date =
-    updatedAt !== createdAt || type === 'update'
-      ? `Updated Review ${new Date(createdAt).toDateString()}`
-      : updatedAt === createdAt && type === 'previous'
-      ? `Previous Review ${new Date(updatedAt).toDateString()}`
-      : `${new Date(updatedAt).toDateString()}`;
+    type === 'update'
+      ? 'Updated review'
+      : type === 'previous'
+      ? 'Previous review'
+      : null;
 
   const handleRemoveClick = (id) => {
     dispatch(removeCommentFromServer(id));
   };
 
-  return (
-    <View>
-      <Text>
-        {title} {typeOutput} {date}
-      </Text>
-      {(props.user && props.user.isAdmin) ||
-      (props.user && props.user.id === user.id) ? (
-        <IconButton
-          style={styles.icon}
-          size={20}
-          icon="delete"
-          onPress={() => handleRemoveClick(id)}
-          accessibilityLabel="close"
-        />
-      ) : null}
-      <Rating
-        type="custom"
-        ratingCount={5}
-        imageSize={20}
-        startingValue={stars}
-        readonly={true}
-        style={styles.starStyle}
+  const updateButton = type === 'update' && (
+    <IconButton
+      size={15}
+      style={styles.updateButton}
+      icon="autorenew"
+      accessibilityLabel="update"
+    />
+  );
+
+  const deleteButton =
+    (user && user.isAdmin) || (user && commentUser.id === user.id) ? (
+      <IconButton
+        style={styles.icon}
+        size={20}
+        icon="delete"
+        onPress={() => handleRemoveClick(id)}
+        accessibilityLabel="close"
       />
+    ) : null;
+
+  const userInformation = type === 'update' && (
+    <View>
       <Image style={styles.userImage} source={{ uri: `${image}` }} />
-      <Paragraph>{`${firstName} ${lastName}`}</Paragraph>
+      <Text>{`${firstName} ${lastName}`}</Text>
+    </View>
+  );
+
+  const reviewPhoto = photo ? (
+    <View style={styles.photoContainer}>
+      <Image source={{ uri: photo }} style={styles.commentImage} />
+    </View>
+  ) : null;
+
+  return (
+    <View style={styles.containerStyle}>
+      <View style={styles.row}>
+        {updateButton}
+        <Text style={styles.typeText}>{typeOutput}</Text>
+        {deleteButton}
+      </View>
+      {userInformation}
+      <View style={styles.row}>
+        <Rating
+          type="custom"
+          ratingCount={5}
+          imageSize={20}
+          startingValue={stars}
+          readonly={true}
+        />
+        <Text style={styles.date}>
+          {'  '}
+          {new Date(updatedAt).toLocaleDateString()}
+        </Text>
+      </View>
+      <Text style={styles.title}>{title}</Text>
       <Paragraph>{comment}</Paragraph>
-      {photo ? (
-        <Image source={{ uri: photo }} style={styles.commentImage} />
-      ) : null}
+      {reviewPhoto}
     </View>
   );
 };
