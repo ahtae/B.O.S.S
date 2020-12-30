a64336e414f007470295d4afca5b779255f727ff
Kristy Li <kristyli20.0@gmail.com>
2020-12-30 14:18:34 -0500

    fix: Duplicate key bug and refactor styling

--- client/src/components/commentsList/comments/Comments.js
+++ client/src/components/commentsList/comments/Comments.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, Image } from 'react-native';
-import { IconButton } from 'react-native-paper';
+import { View, TouchableOpacity, Image } from 'react-native';
 import UpdateComment from './updateComment/UpdateComment';
 import PreviousComments from './previousComments/PreviousComments';
 import styles from '../../../utils/styles/comments';
@@ -16,7 +15,7 @@ const Comments = ({ comments }) => {
 
   const previousComments =
     comments.length > 1 ? (
-      <PreviousComments key={comments[0].id} comments={comments.slice(1)} />
+      <PreviousComments key={comments[1].id} comments={comments.slice(1)} />
     ) : null;
 
   const handleTogglePreviousComments = () => {
@@ -34,11 +33,10 @@ const Comments = ({ comments }) => {
       >
         <Image
           source={showPreviousComments ? ExpandArrow : ExpandButton}
-          style={{ width: 12, height: 12 }}
+          style={styles.icon}
           alt="expand"
         />
       </TouchableOpacity>
-
       {showPreviousComments ? previousComments : null}
     </View>
   );
